36f803531fdde4962b3f6993a7c090aa1ef8cb31
https://github.com/vanvilas <vanessavilas_boas@outlook.com>
2024-04-18 18:14:38 -0300

    add produto no carrinho

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    
-    let preco = quantidade.value * valorUnitario;
-    
-    
-    //calcular o preço, o subtotal
+    let quantidade = document.getElementById('quantidade').value;
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
     //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+
+ 
+    
+    
+    
+    
     //atualizar o valor total
 }
 
